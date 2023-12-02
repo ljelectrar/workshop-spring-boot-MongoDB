38740a6ae6d64f0f3ef7e233e4a926a4636c263d
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-02 14:49:50 -0300

    Data Transfer Object DTO to return User.

--- src/main/java/com/ljelectrar/demospringmongodb/DemospringmongodbApplication.java
+++ src/main/java/com/ljelectrar/demospringmongodb/DemospringmongodbApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemospringmongodbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemospringmongodbApplication.class, args); 
+		SpringApplication.run(DemospringmongodbApplication.class, args);
 	}
 
 }
--- src/main/java/com/ljelectrar/demospringmongodb/resources/UserResource.java
+++ src/main/java/com/ljelectrar/demospringmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ljelectrar.demospringmongodb.resources;
 
 import com.ljelectrar.demospringmongodb.domain.User;
+import com.ljelectrar.demospringmongodb.dto.UserDTO;
 import com.ljelectrar.demospringmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET) // or @getMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
          List<User> list = service.findAll();
-         return ResponseEntity.ok().body(list);
+         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+         return ResponseEntity.ok().body(listDto);
     }
 }
