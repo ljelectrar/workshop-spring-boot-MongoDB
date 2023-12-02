5904ece49c79ca98fc3d5475537e593ccd58a08c
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-02 09:48:31 -0300

    Project created.

---
+++ src/main/java/com/ljelectrar/demospringmongodb/DemospringmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ljelectrar.demospringmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljelectrar/demospringmongodb/DemospringmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljelectrar.demospringmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
