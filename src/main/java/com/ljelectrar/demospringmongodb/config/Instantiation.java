2da0b10f62074c832d709578b184f60c326a575b
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-02 21:52:16 -0300

    Refering user's posts.

--- src/main/java/com/ljelectrar/demospringmongodb/config/Instantiation.java
+++ src/main/java/com/ljelectrar/demospringmongodb/config/Instantiation.java
@@ -43,5 +43,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("31/04/2015"),"Partiu volta", "Vou voltar para o Rio!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
