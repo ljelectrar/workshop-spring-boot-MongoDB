4ba3a4b0e7a644823a8ad175cd7cfcc0ffa2405c
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-02 23:28:27 -0300

    Query with a lot of requests.

--- src/main/java/com/ljelectrar/demospringmongodb/resources/PostResource.java
+++ src/main/java/com/ljelectrar/demospringmongodb/resources/PostResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,4 +33,19 @@ public class PostResource {
         List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body(list);
     }
+
+    @RequestMapping(value = "/fullsearch" ,method = RequestMethod.GET) // or @getMapping
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text",  defaultValue="") String text,
+            @RequestParam(value="minDate",  defaultValue="") String minDate,
+            @RequestParam(value="maxDate",  defaultValue="") String maxDate) {
+        text = URL.decodeParam(text);
+
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date(0L));
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 }
--- src/main/java/com/ljelectrar/demospringmongodb/services/PostService.java
+++ src/main/java/com/ljelectrar/demospringmongodb/services/PostService.java
@@ -9,6 +9,7 @@ import com.ljelectrar.demospringmongodb.services.exception.ObjectNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repo.findByTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date min, Date max){
+        max = new Date(max.getTime() + 24 * 60 *60 *1000);
+        return repo.fullSearch(text, min, max);
     }
 
 }
