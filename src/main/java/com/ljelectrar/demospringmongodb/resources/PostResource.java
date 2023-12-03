c9b994694f73a9c9207f8588a72b91f8008c011f
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-02 22:51:11 -0300

    Simple query , query methods.

--- src/main/java/com/ljelectrar/demospringmongodb/resources/PostResource.java
+++ src/main/java/com/ljelectrar/demospringmongodb/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.ljelectrar.demospringmongodb.resources;
 import com.ljelectrar.demospringmongodb.domain.Post;
 import com.ljelectrar.demospringmongodb.domain.User;
 import com.ljelectrar.demospringmongodb.dto.UserDTO;
+import com.ljelectrar.demospringmongodb.resources.util.URL;
 import com.ljelectrar.demospringmongodb.services.PostService;
 import com.ljelectrar.demospringmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/searchtitle" ,method = RequestMethod.GET) // or @getMapping
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text",  defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/ljelectrar/demospringmongodb/services/PostService.java
+++ src/main/java/com/ljelectrar/demospringmongodb/services/PostService.java
@@ -18,10 +18,13 @@ public class PostService {
     @Autowired
     private PostRepository repo;
 
-
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
