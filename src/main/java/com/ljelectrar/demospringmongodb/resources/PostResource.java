bfb601f241bfb1cc38d7ddc327d0aa2e22df90d2
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-02 22:08:38 -0300

    Getting a post by id.

---
+++ src/main/java/com/ljelectrar/demospringmongodb/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.ljelectrar.demospringmongodb.resources;
+
+import com.ljelectrar.demospringmongodb.domain.Post;
+import com.ljelectrar.demospringmongodb.domain.User;
+import com.ljelectrar.demospringmongodb.dto.UserDTO;
+import com.ljelectrar.demospringmongodb.services.PostService;
+import com.ljelectrar.demospringmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired // Injection dependency using the framework
+    private PostService service;
+
+    @RequestMapping(value = "/{id}" ,method = RequestMethod.GET) // or @getMapping
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/ljelectrar/demospringmongodb/services/PostService.java
@@ -0,0 +1,27 @@
+ package com.ljelectrar.demospringmongodb.services;
+
+import com.ljelectrar.demospringmongodb.domain.Post;
+import com.ljelectrar.demospringmongodb.domain.User;
+import com.ljelectrar.demospringmongodb.dto.UserDTO;
+import com.ljelectrar.demospringmongodb.repository.PostRepository;
+import com.ljelectrar.demospringmongodb.repository.UserRepository;
+import com.ljelectrar.demospringmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+
+}
